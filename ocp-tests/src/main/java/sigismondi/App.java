7b8ff45e428a043dc72c7fdcd130907a3a02d507
msigismondi <marco.sigismondi.external@capgemini.com>
2017-05-16 15:11:31 +0200

    Primo glorioso commit

---
+++ ocp-tests/src/main/java/sigismondi/App.java
@@ -0,0 +1,13 @@
+package sigismondi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
